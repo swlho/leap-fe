45d122aa4553f56accca0a9238fdeafe397ed42e
swlho <swlho87@gmail.com>
2024-03-26 09:31:20 +0000

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,13 +11,16 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Leap.</strong>
-              <br></br><br></br>
-              An AI-powered learning platform to help you leap before you learn.<br></br><br></br>
-              Get up to speed with any topic.  Connect with fellow Leapers to take your learning further
+            <strong>Log in to your account</strong>
+              <form>
+                <label htmlFor='email-field'>Email </label>
+                <input type='email' id='email-field'></input><br></br>
+                <label htmlFor='password-field'>Password </label>
+                <input type='password' id='password-field'></input>
+              </form>
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -8,7 +8,7 @@ export default function SideNav() {
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
+        href="/dashboard"
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
@@ -18,10 +18,10 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <Link className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3" href={'/'}>
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
-          </button>
+          </Link>
         </form>
       </div>
     </div>
