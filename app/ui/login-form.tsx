fac5c2e43e736089117ffdf67c7a1a5bdbc0d624
Mariha-B <mariha_butt@hotmail.com>
2024-04-04 13:44:00 +0100

    Changed placeholder to username

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -29,7 +29,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="Enter your username"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
