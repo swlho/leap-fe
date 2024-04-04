bc8f813acf7e6c57f91408968e553565d7396a28
swlho <swlho87@gmail.com>
2024-04-04 12:40:03 +0100

    added loader to AI

--- app/ui/dashboard/DashboardContainer.tsx
+++ app/ui/dashboard/DashboardContainer.tsx
@@ -13,15 +13,14 @@ export default function DashboardContainer(){
     const [result,setResult] = useState(null)
     const [isFirstRequest, setIsFirstRequest] = useState(false)
     const [summaryDone, setSummaryDone] = useState(false)
+    const [isLoading,setIsLoading] = useState(false)
 
 return (
 <>
-  <SearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} setResultFound={setResultFound} setResult={setResult} setSummaryDone={setSummaryDone} setIsFirstRequest={setIsFirstRequest}/>
-  <ResultContainer resultFound={resultFound} result={result} summaryDone={summaryDone} setSummaryDone={setSummaryDone} isFirstRequest={isFirstRequest} searchTerm={searchTerm}/>
+  <SearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} setResultFound={setResultFound} setResult={setResult} setSummaryDone={setSummaryDone} setIsFirstRequest={setIsFirstRequest} setIsLoading={setIsLoading}/>
+  <ResultContainer resultFound={resultFound} result={result} summaryDone={summaryDone} setSummaryDone={setSummaryDone} isFirstRequest={isFirstRequest} searchTerm={searchTerm} isLoading={isLoading}/>
   <p className="text-2xl m-5 font-lemonRegular">Your Recommended Ponds</p>
   <RecommendedPondsContainer/>
   </>
 )
-
-
-
+}
---
+++ app/ui/loadingAI.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoadingAI: React.FC = () => {
+  return <span className="loading loading-ring loading-lg text-info"></span>
+};
+
+export default LoadingAI;
