8b38b52c40cbefc81f2716ad9e1d5259fd7f681a
swlho <swlho87@gmail.com>
2024-04-05 16:54:47 +0100

    refactored to optimistic render

--- app/ui/dashboard/CreateJoinButton.tsx
+++ app/ui/dashboard/CreateJoinButton.tsx
@@ -21,41 +21,27 @@ export default function CreateJoinButton({ children, className, isFirstRequest, 
   let [buttonText,setButtonText] = useState(isFirstRequest? 'Create & Join' : 'Join')
 
   const handleClick = (event:any) =>{
+    event.target.disabled=true
+    setButtonText('Joined')
+    setButtonClicked(clsx('flex h-10 items-center rounded-lg bg-green-400 px-4 text-sm font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'))
     getUserById('660d70386114563bf754fb5d')
-    .then(({data})=>{
-      const userTopicsArr = data[0].user_topics
-      return userTopicsArr
-    })
-    .then((userTopicsArr)=>{
-      console.log(id,"topic id")
+    .then(()=>{
       putUserById('660d70386114563bf754fb5d','mkelling0','rdimbleby0@surveymonkey.com', id)
-      .then(()=>{
-        console.log("user updated")
-        setButtonClicked(clsx('flex h-10 items-center rounded-lg bg-green-400 px-4 text-sm font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'))
-        event.target.disabled=true
-        setButtonText('Joined')
-      })
+      console.log("user updated")
     })
   }
 
   const handleClickToCreateAndJoin = (event:any) =>{
+    event.target.disabled=true
+    setButtonText('Joined')
+    setButtonClicked(clsx('flex h-10 items-center rounded-lg bg-green-400 px-4 text-sm font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'))
     postPond(searchTerm)
     .then(()=>{
-      console.log("new pond created")
       getUserById('660d70386114563bf754fb5d')
-      .then(({data})=>{
-        const userTopicsArr = data[0].user_topics
-        return userTopicsArr
-      })
-      .then((userTopicsArr)=>{
-        console.log(id,"topic id")
+      .then(()=>{
         putUserById('660d70386114563bf754fb5d','mkelling0','rdimbleby0@surveymonkey.com', id)
-        .then(()=>{
-          console.log("user updated")
-          setButtonClicked(clsx('flex h-10 items-center rounded-lg bg-green-400 px-4 text-sm font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'))
-          event.target.disabled=true
-          setButtonText('Joined')
-        })
+        console.log("new pond created")
+        console.log("user updated")
       })
     })
   }
